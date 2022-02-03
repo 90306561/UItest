3751ffac29a7de87ca4779bd7b043d2c701a78ae
90306561 <72754329+90306561@users.noreply.github.com>
2022-02-03 10:21:49 -0600

    Initial Commit

---
+++ UItest/UItestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UItestApp.swift
+//  UItest
+//
+//  Created by 90306561 on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct UItestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
